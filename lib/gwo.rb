dd8244597a30060355ddad969fc2ce8016a91369
Tim Underwood <timunderwood@gmail.com>
2009-03-23 16:54:10 -0700

    Integrated Eric Vasilik's technique, added gwo_for helper, updated GA tracking
    - Integrated Eric Vasilik's (http://gwotricks.com/) advanced technique
      (described in README) for handling dynamic content with GWO
    
    - Added GWO::Experiment class and gwo_for helper for DRYer usage
    
    - Updated GA tracking to use the new ga.js code and _gat object

--- lib/gwo.rb
+++ lib/gwo.rb
@@ -1,100 +1,80 @@
 module GWO
   module Helper
-    def gwo_start(id)
-      javascript_tag(%{
-        function utmx_section(){}function utmx(){}
-        (function(){var k='#{id}',d=document,l=d.location,c=d.cookie;function f(n){
-        if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return c.substring(i+n.
-        length+1,j<0?c.length:j)}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;
-        d.write('<sc'+'ript src="'+
-        'http'+(l.protocol=='https:'?'s://ssl':'://www')+'.google-analytics.com'
-        +'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='
-        +new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
-        '" type="text/javascript" charset="utf-8"></sc'+'ript>')})();
-      })
+    def gwo_for(options = {}, &block)
+      options[:erb_context] = self
+      GWO::Experiment.new(options, &block)
     end
     
-    def gwo_end(uacct, id)
-      javascript_tag(%{
-        if(typeof(urchinTracker)!='function')document.write('<sc'+'ript src="'+
-        'http'+(document.location.protocol=='https:'?'s://ssl':'://www')+
-        '.google-analytics.com/urchin.js'+'"></sc'+'ript>')
-        </script>
-        <script>
-        try {
-        _uacct = #{uacct.inspect};
-        urchinTracker("/#{id}/test");
-        } catch (err) { }
-      })
+    def gwo_start(experiment_id)
+      %{<script type="text/javascript">
+function utmx_section(){}function utmx(){}
+(function(){var k='#{experiment_id}',d=document,l=d.location,c=d.cookie;function f(n){
+if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return c.substring(i+n.
+length+1,j<0?c.length:j)}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;
+d.write('<sc'+'ript src="'+
+'http'+(l.protocol=='https:'?'s://ssl':'://www')+'.google-analytics.com'
++'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='
++new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
+'" type="text/javascript" charset="utf-8"></sc'+'ript>')})();
+</script>
+      }
     end
     
-    def gwo_conversion(uacct, id)
-      javascript_tag(%{
-        if(typeof(urchinTracker)!='function')document.write('<sc'+'ript src="'+
-        'http'+(document.location.protocol=='https:'?'s://ssl':'://www')+
-        '.google-analytics.com/urchin.js'+'"></sc'+'ript>')
-        </script>
-        <script>
-        try {
-        _uacct = #{uacct.inspect};
-        urchinTracker("/#{id}/goal");
-        } catch (err) { }
-      })
+    def gwo_end(experiment_id, ga_acct)
+      %{<script type="text/javascript">
+if(typeof(_gat)!='object')document.write('<sc'+'ript src="http'+
+(document.location.protocol=='https:'?'s://ssl':'://www')+
+'.google-analytics.com/ga.js"></sc'+'ript>')</script>
+<script type="text/javascript">
+try {
+var pageTracker=_gat._getTracker("#{ga_acct}");
+pageTracker._trackPageview("/#{experiment_id}/test");
+}catch(err){} </script>
+      }
     end
     
-    def gwo_static_section(name, &block)
-      concat(script(name) { capture(&block) })
+    def gwo_conversion(experiment_id, ga_acct)
+      %{<script type="text/javascript">
+if(typeof(_gat)!='object')document.write('<sc'+'ript src="http'+
+(document.location.protocol=='https:'?'s://ssl':'://www')+
+'.google-analytics.com/ga.js"></sc'+'ript>')</script>
+<script type="text/javascript">
+try {
+var pageTracker=_gat._getTracker("#{ga_acct}");
+pageTracker._trackPageview("/#{experiment_id}/goal");
+}catch(err){} </script>
+      }
     end
     
-    def gwo_section(name, html_options = {}, &block)
-      concat(
-        content_tag(:div, 
-          capture(&block), 
-          html_options.merge({
-            :id    => "gwo_#{name.to_s}",
-            :style => "display:none"
-          })
-        )
-      )
+    def gwo_section_control(section_name, content = nil, &block)
+      concat(%{<!-- utmx section name="#{section_name}" -->
+<script type="text/javascript">
+var gwoVariation = utmx("variation_number", "#{section_name}");
+if(gwoVariation != undefined && gwoVariation != 0) document.write("<noscript>");
+</script>
+})
+      
+      content ||= ''
+      content += capture(&block) if block_given?
+      # TODO strip or rewrite <noscript> tags
+      concat(content)
+      
+      concat(%{\n</noscript>\n})
     end
     
-    def gwo_dynamic_end(default, uacct, id)
-      javascript_tag(%{
-        function GWO(name){
-          document.getElementById("gwo_" + name).style.display = 'block';
-        }
-      }) + 
-      script(default) { 
-        javascript_tag("GWO(#{default.to_s.inspect})")
-      } + 
-      gwo_end(uacct, id)
+    def gwo_section_variation(section_name, variation_number, content = nil, &block)
+      concat(%{<script type="text/javascript">
+var gwoVariation = utmx("variation_number", "#{section_name}");
+if(gwoVariation != undefined && gwoVariation == #{variation_number}) document.write('</noscript a="')</script> <!-- " >
+})
+      
+      content ||= ''
+      content += capture(&block) if block_given?
+      # TODO strip or rewrite HTML comments
+      concat(content)
+      
+      concat(%{<script type="text/javascript"> document.write('<' + '!' + '-' + '-'); </script>  -->
+})
     end
-    
-    def gwo(default, uacct, id)
-      gwo_start(id) + gwo_dynamic_end(default, uacct, id)
-    end
-    
-    private
-    
-      # I'm overriding this since GWO doesn't like the CDATA section for some reason...
-      def javascript_tag(content_or_options_with_block = nil, html_options = {}, &block)
-        content =
-          if block_given?
-            html_options = content_or_options_with_block if content_or_options_with_block.is_a?(Hash)
-            capture(&block)
-          else
-            content_or_options_with_block
-          end
-        tag = content_tag(:script, content, html_options.merge(:type => Mime::JS))
-        if block_called_from_erb?(block)
-          concat(tag)
-        else
-          tag
-        end
-      end
-    
-      def script(name, &block)
-        javascript_tag("utmx_section(#{name.to_s.inspect})") + yield + "</noscript>"
-      end
   end
 end
---
+++ lib/gwo/experiment.rb
@@ -0,0 +1,91 @@
+module GWO
+  class Experiment
+    attr_writer :experiment_id, :ga_acct
+    
+    # GWO::Experiment.new(:experiment_id => '123123', :ga_acct => 'UA-123-123')
+    def initialize(options = {})
+      @experiment_id = options[:experiment_id]
+      @ga_acct = options[:ga_acct]
+      @erb_context = options[:erb_context]
+      render(){ yield(self) } if block_given?
+      self
+    end
+    
+    # Returns the current experiment_id value or sets it if a value is passed
+    #
+    # Allows you to do something like:
+    #   GWO::Experiment.new().experiment_id('123123').ga_acct('UA-123-123').render do |exp| ...
+    #
+    def experiment_id(experiment_id = nil)
+      return @experiment_id unless experiment_id      
+      @experiment_id = experiment_id
+      self
+    end
+    
+    # Returns the current ga_acct value or sets it if a value is passed
+    def ga_acct(ga_acct = nil)
+      return @ga_acct unless ga_acct
+      @ga_acct = ga_acct
+      self
+    end
+    
+    def erb_context(erb_context = nil)
+      return @erb_context unless erb_context
+      @erb_context = erb_context
+      self
+    end
+    
+    def render(&block)
+      yield(self)
+      @erb_context.concat self.end()
+    end
+    
+    def start()
+      return if @start_called # Start only needs to be called once
+      @start_called = true
+      @erb_context.gwo_start(@experiment_id)
+    end
+    
+    def end()
+      return if @end_called
+      @end_called = true
+      @erb_context.gwo_end(@experiment_id, @ga_acct)
+    end
+    
+    def conversion()
+      @erb_context.gwo_conversion(@experiment_id, @ga_acct)
+    end
+    
+    def section(name)
+      start()
+      section = Section.new(self, name)
+      yield(section) if block_given?
+      section
+    end
+    
+    def section_control(name, content = nil, &block)
+      start()
+      @erb_context.gwo_section_control(name, content, &block)
+    end
+    
+    def section_variation(name, variation_number, content = nil, &block)
+      @erb_context.gwo_section_variation(name, variation_number, content, &block)
+    end
+    
+  end
+  
+  class Section
+    def initialize(experiment, name)
+      @experiment = experiment
+      @name = name
+    end
+    
+    def control(content = nil, &block)
+      @experiment.section_control(@name, content, &block)
+    end
+    
+    def variation(variation_number, content = nil, &block)
+      @experiment.section_variation(@name, variation_number, content, &block)
+    end
+  end
+end
